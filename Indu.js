2f908f86593794967d1c21b131ebb6861f1d6ee9
Indu <indurevathy@gmail.com>
2021-08-02 14:27:38 +0200

    change updated

---
+++ Indu.js
@@ -0,0 +1,22 @@
+ausgabe(Addition(2,1));
+ausgabe(Subtraction(2,1));
+ausgabe(Multipilication(2,3));
+ausgabe(Division(2,2));
+function Addition(a,b){
+    return a+b;
+}
+function Subtraction(a,b){
+    return a-b;
+}
+function Multipilication(a,b){
+    return a*b;
+}
+function Division(a,b){
+    return a/b;
+}
+
+
+
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
--- script_01.js
+++ script_01.js
@@ -1,18 +1,1 @@
 
-/*** Rechner */
-/*
-0. a+b / a-b/ a*b / a/b  // ergebnis c
-1. Dateneingabe + -überprüfung 
-2. Auswahl Rechenart 
-3. Fkt. Grundrechenarten 
-4. Ausgabe in Konsole 
-*/
-
-
-
-// Modul: Konsolenausgabe |  Test:
-ausgabe("Hallo Welt!")
-ausgabe(20);
-function ausgabe(outputStr) {
-    console.log(outputStr);
-}
